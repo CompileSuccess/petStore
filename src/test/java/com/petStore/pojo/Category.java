0cf56d48781ef51f20713d1273f447c9afaeaeac
OlgaGrynyk <olgagrynyk@hotmail.com>
2022-05-08 19:25:39 -0400

    PetAutomation

---
+++ src/test/java/com/petStore/pojo/Category.java
@@ -0,0 +1,9 @@
+package com.petStore.pojo;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private int id;
+    private String name;
+}
---
+++ src/test/java/com/petStore/pojo/Tag.java
@@ -0,0 +1,10 @@
+package com.petStore.pojo;
+
+
+import lombok.Data;
+
+@Data
+public class Tag {
+    private int id;
+    private String name;
+}
